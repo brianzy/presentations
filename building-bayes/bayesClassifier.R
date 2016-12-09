ea61963f46db7f6d5e5b5103b7193d7116b28560
b0dh1sattva <tim.dunbar@gmail.com>
2016-12-09 16:24:32 -0500

    add comments to both files and changed the quote for testing, working on accounting for differences in pop size

--- building-bayes/bayesClassifier.R
+++ building-bayes/bayesClassifier.R
@@ -1,18 +1,27 @@
-bayesClassifier<-function(menq, womenq, quote, prior){
-  wCount<-nrow(womenq)
-  mCount<-nrow(menq)
-  menq<-as.data.frame(table(menq))
-  womenq<-as.data.frame(table(womenq))
-  newM<-menq[is.element(menq$m, intersect(menq$m, quote$`unlist(x)`)),]
-  newW<-womenq[is.element(womenq$w, intersect(womenq$w, quote$`unlist(x)`)),]
-  newM$Freq<-newM$Freq/mCount
-  newW$Freq<-newW$Freq/wCount
+bayesClassifier<-function(menQ, womenQ, quote, menP, womenP){
+  #gets row count of men and women data frames
+  wCount<-nrow(womenQ)
+  mCount<-nrow(menQ)
+  #adds frequency column to men and women data frames
+  menQ<-as.data.frame(table(menQ))
+  womenQ<-as.data.frame(table(womenQ))
+  #finds intersection of quote data frame and the above data frames
+  newM<-menQ[is.element(menQ$m, intersect(quote$`unlist(x)`, menQ$menQ)),]
+  newW<-womenQ[is.element(womenQ$w, intersect(quote$`unlist(x)`, womenQ$womenQ)),]
+  #finds chance of occurance of words in quote in each data frame (womenQ and MenQ)
+  #working on accounting for population size here as well
+  newM$Freq<-newM$Freq/mCount*(mCount/(mCount+wCount))
+  newW$Freq<-newW$Freq/wCount*(wCount/(mCount+wCount))
+  #sums the frequency columns to come up with the likelihoods
   likelihoodW<-sum(newW$Freq)
   likelihoodM<-sum(newM$Freq)
-  posteriorM<-prior*likelihoodM
-  posteriorW<-prior*likelihoodW
+  #multiplies likelihoods by priors to get posteriors
+  posteriorM<-menP*likelihoodM
+  posteriorW<-womenP*likelihoodW
+  #test for higher posterior
   if(posteriorW>posteriorM){
     return("Woman")
   }
   return("Man")
 }
+
